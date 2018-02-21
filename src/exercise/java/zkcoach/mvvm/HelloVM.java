7ef81bee9d8cc826221188e5738a6656c108e0b5
hawkhero <hawkhero@gmail.com>
2018-02-21 18:39:03 +0800

    - add exercise marker - add exercise file

---
+++ src/exercise/java/zkcoach/mvvm/HelloVM.java
@@ -0,0 +1,13 @@
+package zkcoach.mvvm;
+
+import org.zkoss.bind.annotation.*;
+
+
+public class HelloVM {
+	private String hello = "hello";
+
+	//TODO, 3, getter
+
+	//TODO, 4, command method
+
+}
--- src/main/java/zkcoach/mvvm/HelloVM.java
+++ src/main/java/zkcoach/mvvm/HelloVM.java
@@ -4,13 +4,14 @@ import org.zkoss.bind.annotation.*;
 
 
 public class HelloVM {
-
 	private String hello = "hello";
 
+	//TODO, 3, getter
 	public String getHello() {
 		return hello;
 	}
 
+	//TODO, 4, command method
 	@Command @NotifyChange("hello")
 	public void helloWorld(){
 		hello = "hello world";
